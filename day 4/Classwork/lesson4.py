bcab2a0e769090133fda507b7703743dc0277ad4
datickhkikvadze <datimine1@gmail.com>
2023-12-02 17:45:43 +0400

    fourth commit

---
+++ day 4/Classwork/lesson4.py
@@ -0,0 +1,26 @@
+# age = 12
+# age = "12"
+# name = "dati"
+
+#print("Great Progres")
+
+# salary = 50000
+# role = "Analyst"
+# age = 29
+# print(salary)
+
+
+# name = "Dati"
+# print(nAme)
+
+
+
+# player1_name = "Dati"
+
+#2nduser = eror
+
+# user*name = "eror"
+
+
+user_name = input("wthat's your name? : ")
+print ("his name is " + user_name )
---
+++ day 4/Homework/lesson4.py
@@ -0,0 +1,30 @@
+#variables for my family
+mum = 38
+dad = 40
+bro1 = 6
+bro2 = 14
+#variables for my family(20 years later)
+mum = 58
+dad = 60
+bro1 = 26
+bro2 = 34
+#lets print all this
+print("my family ages 20 years later!!!")
+print(mum)
+print(dad)
+print(bro1)
+print(bro2)
+
+
+#Now lets have a little quiz!!!
+
+Mum_age = input("wthat's mum's age? :")
+print("ready for next!!!")
+Dad_age= input("wthat's dad's age? :")
+print("ready for next!!!")
+Bro1_age= input("wthat'Bro1's age? :")
+print("ready for next!!!")
+Bro2_age= input("wthat'Bro2's age? :")
+print("Goodjob! you did it!!!!!!!!!!!!!!!!")
+
+    

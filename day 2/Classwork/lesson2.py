3adf41f07dc1d988cc0dced968b81aeedf88f96f
datickhkikvadze <datimine1@gmail.com>
2023-12-02 12:24:59 +0400

    third commit

---
+++ day 2/Classwork/lesson2.py
@@ -0,0 +1,49 @@
+# display("gasatestad tu mushaobs display() dd")
+
+
+
+
+#PC_pirce = 10000
+#print(available_money - PC_pirce * (100 - discount) / 100) 
+
+
+
+
+
+#second variant
+
+
+
+
+#ცვლადები
+# PC_price = 10000
+# discount = 10
+# available_money = 9600
+
+#რამდენი დააკლდა კომპიუტერს
+# discounted_price = PC_price * discount / 100
+
+#რამდენი დარჩა
+# updatedPCprice = PC_price - discounted_price
+
+#შედეგი
+# result = available_money - updatedPCprice
+
+# print(int(result)) 
+
+#რომ გავიგოთ რიცხვის პროცენტი, ეს რიცხვი უნდა გავყოთ 100ზე 
+#და გავამრავლოტ მოცემულ პროზენტზე.
+#10000/100 * 10 = 1000
+
+# print(7 + 3)
+# print(10 - 5)
+# print(15 / 2)
+# print(15 // 2 )
+# print(84 % 9)
+
+
+# budget = 200
+# budget = 300
+# print(budget)
+
+
---
+++ day 2/Homework/lesson2.py
@@ -0,0 +1,14 @@
+#variables for my family members
+mum = 38
+dad = 40
+me = 12
+brother1 = 14
+brother2 = 6
+#me da mamachemis asaki 10 wlis shemdeg
+me = me + 10
+dad = dad +10
+
+#gamoitvalet ramdenjer aghemateba tqveni mamis asaki tqvens asaks 10 wlis shemdeg
+
+
+print(dad / me )

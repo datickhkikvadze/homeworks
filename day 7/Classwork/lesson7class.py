793862a4f2358a2c9e7cb90da9ee10f0d08bec57
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2023-12-06 21:10:35 +0400

    Add files via upload

---
+++ day 7/Classwork/lesson7class.py
@@ -0,0 +1,23 @@
+# i=4
+# while i<=20:
+#     print(i)
+#     i+=4
+
+
+# number = 2
+# sum = 0
+
+# while number <= 100:
+#     sum += number
+#     number += 2
+# print("Sum of even numbers from 1 to 100:", sum)
+# print(sum)
+
+i = 0
+while True:
+    i+=1
+    if i == 4:
+        continue
+    if i == 6:
+        break
+    print(i)
---
+++ day 7/Homework/lesson7home.py
@@ -0,0 +1,9 @@
+Ticket_price =25 #free for kids
+adamianebi =0
+while True:
+    adamianebi+=1
+    if adamianebi == 3:
+        continue
+    if  adamianebi == 13:
+        break
+    print(adamianebi * Ticket_price - 3 * Ticket_price)

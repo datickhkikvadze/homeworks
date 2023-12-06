8942ea29db6fdc17748fe348f22c46f9a1b10a60
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2023-12-06 15:17:55 +0400

    Add files via upload

---
+++ day 6/lesson6.py
@@ -0,0 +1,19 @@
+#1-30 numbers!!
+
+i=0
+a=1
+while i<=30:
+    print(i, " -luwi")
+    i=i+2
+    print(a, "-kenti")
+    a=a+2
+print("Finish!!")
+
+
+#my dayly rutine
+
+
+daily_routine="wake up, eating, going school, lunch, back from school,doing homework,comming back at home, learning programming and sleep"
+while daily_routine>=str():
+    print(daily_routine)
+

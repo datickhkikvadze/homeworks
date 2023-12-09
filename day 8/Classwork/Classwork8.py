b4d636696335dcb8c296cf4af67c6c53facb4c83
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2023-12-09 15:12:40 +0400

    Add files via upload

---
+++ day 8/Classwork/Classwork8.py
@@ -0,0 +1,68 @@
+# test = [1,23,58,"test","next is float",38.1]
+# print(test [0] + test[1] + test[2] +   test[5])
+
+
+# m = [
+#     [1, 2, 3],
+#     [4, 5, 6]
+#     ]
+# print (m) 
+
+
+# my_str = "Hello World"
+#          #012345678910   
+# print(my_str[6])
+
+
+
+# nums = [ 5 , 8 , 2]
+# nums[1]= 42
+# print(nums)
+
+# nums = [1,2,3,4,5]
+# nums[3] = nums[1]
+# print(nums[3])
+
+
+# nums= [1,2,3]
+# print(nums + [4,5,6])
+
+
+
+# i = [1,2,3]
+
+# i+= [6,8,9]
+# print(i[5] // i[1])
+
+# x = [2,4]
+# x*=3
+# print(x[3])
+
+# words = ["dati","gio","temo"]
+# print("dati"in words)
+# print("luka" in words)
+# print("gio" in words )
+# print("temo" in words)
+
+# nums = [10,9,8,7,6,5]
+# nums[0]=nums[1]-5
+# if 4 in nums:
+#     print(nums[3])
+# else:
+#     print(nums[4])
+    
+# nums=[1,2,3]
+# print(not 4 in nums)
+# print(4 not in nums)
+# print(not 3 in nums)
+# print(3 not in nums)
+
+# for i in range (1,20,3):
+#     print(i)
+
+# for char in "დათი" :
+#     print(char)
+
+# My_list = ["dati","gio","temo",]
+# for elem in My_list:
+#     print(elem)

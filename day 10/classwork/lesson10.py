7893c4239b49f3a6736c3ff2ca5930cac925a9f3
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2023-12-11 18:10:27 +0400

    Add files via upload

---
+++ day 10/classwork/lesson10.py
@@ -0,0 +1,60 @@
+# nums =[1,2,3,4,5,6,6]
+# print(len(nums))
+
+
+# surname = "chkhikvadze"
+# print(len(surname))
+
+# set_of_nums=set(nums)
+# print(set_of_nums)
+
+# letters = ["a","b","c"]
+# letters += ["d"]
+# print(len(letters))
+# print(letters)
+
+# nums = [1,2,3]
+# nums.append(4) #amatebs siis bolashi gadacemul elements
+# print(nums)
+
+
+# words = ["Python","Fun"]
+#           #0
+# words.insert(1,"is")#amatebs sasurvel elesments siis sasurvel poziciaze
+#    #sad chajdes#ra chajdes 
+# print(words)
+
+# family=["sofo","ako"]
+# family.append("dati")
+# family.insert(1,"temo")
+# print(family)
+
+# nums = [9,8,7,6,5]
+# nums.append(4)
+# nums.insert(2,11)
+# print(len(nums))]
+
+# letters = ["p","q","r","s","p","u"]
+
+# print(letters.index("r"))
+# print(letters.index("p"))
+# print(letters.index("q"))
+
+# x = [1,8,42,3]
+# print(min(x))
+# print(max(x))
+
+# x=[1,2,2,5,7,5,9,8,4,3,2,7,5,1,4,9,6,8]
+# print(x.count(2))
+# x.remove(5)
+# print(x)
+# x.reverse()
+# print(x)
+
+# nums = [4,5,6]
+# msg = "Numbers: {0} {1} {2} ".format (nums[1],nums[1],nums[2])
+# print(msg)
+
+# family = ["sofo","ako","dati","gio","temo"]
+# fml = "Family_Members: {0} {1} {2} {3} {4}  ".format (family[0],family[1],family[2],family[3],family[4])
+# print(fml)

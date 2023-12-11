e62d51a43c4fde05e2766cb9e6ff8942e5738cc8
datickhkikvadze <152197029+datickhkikvadze@users.noreply.github.com>
2023-12-11 18:09:14 +0400

    Add files via upload

---
+++ day 9/Classwork/lesson9.py
@@ -0,0 +1,51 @@
+# from turtle import *
+# def kvadratis_mxatvari_kodi():
+#   for i in range (4):
+#     forward(100)
+#     left(90)
+# def kalmis_wageba (x,y):
+#     penup()
+#     goto(x,y)
+#     pendown()
+# #1-st square
+# kvadratis_mxatvari_kodi()
+# kalmis_wageba (0,200)
+# #2-st square
+# kvadratis_mxatvari_kodi()
+# kalmis_wageba(-200,200)
+# #3-st square
+# kvadratis_mxatvari_kodi()
+# kalmis_wageba (-200,0)
+# #4-st square
+# kvadratis_mxatvari_kodi()
+# exitonclick()
+
+
+
+# # def biwebtan_misalmeba ():
+# #     print("welcome dati")
+# #     print("welcome gio")
+# #     print("welcome temo")
+# # print("dilis misalmeba")
+# # biwebtan_misalmeba ()
+# # print("shuadgis misalmeba")
+# # biwebtan_misalmeba ()
+# # print("sagamos misalmeba")
+# # biwebtan_misalmeba ()
+
+
+
+# print("sheiyvane datis asaki 2 jer!!!")  
+
+# age = input()
+# def misalmeba(saxeli,age):
+#     print(" mogesalmebi " + saxeli)
+#     print("sheni asakia", age)
+
+# misalmeba(" dati",input())
+# print("sheiyvane gios asaki!!!")  
+# misalmeba(" gio",input())
+# print("sheiyvane temos asaki!!!")  
+# misalmeba(" temo",input())
+
+# print("shen ukve ici bavshvebis sakebi")
